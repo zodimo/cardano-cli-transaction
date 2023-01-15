fbbefe734a5cc84a0bdfc2996690246d7a39920b
Jaco Labuschagne <zodimo@gmail.com>
2023-01-15 13:34:17 +0200

    script-valid

---
+++ src/command/buildParameters/script-valid.ts
@@ -0,0 +1,27 @@
+import { BooleanCommandParameter } from '@zodimo/cardano-cli-base';
+
+export enum ScriptValidOptions {
+  IS_VALID = 'script-valid',
+  IS_INVALID = 'script-invalid',
+}
+
+export class ScriptValidBuilder {
+  isValid(): ScriptValid {
+    return ScriptValid.isValid();
+  }
+  isInvalid(): ScriptValid {
+    return ScriptValid.isInvalid();
+  }
+}
+
+export class ScriptValid extends BooleanCommandParameter {
+  constructor(paramKey: ScriptValidOptions) {
+    super(paramKey);
+  }
+  static isValid(): ScriptValid {
+    return ScriptValid.from(ScriptValidOptions.IS_VALID);
+  }
+  static isInvalid(): ScriptValid {
+    return ScriptValid.from(ScriptValidOptions.IS_INVALID);
+  }
+}

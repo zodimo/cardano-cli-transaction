3746aeb5a4006ea6dfc12d3146d6864d3c8cdd78
Jaco Labuschagne <zodimo@gmail.com>
2023-01-15 12:09:59 +0200

    some transaction tests

--- test/transaction.spec.ts
+++ test/transaction.spec.ts
@@ -1,3 +1,6 @@
+import { BuildOptions } from '../src/command/build';
+import { SignOptions } from '../src/command/sign';
+import { SubmitOptions } from '../src/command/submit';
 import { Transaction } from '../src/transaction';
 
 describe('cardano-cli transaction', () => {
@@ -7,5 +10,29 @@ describe('cardano-cli transaction', () => {
 
   it('can change cliBinPath commandPrefix', () => {
     expect(Transaction.createWithCardanoCliBin('cli').commandPrefix).toBe('cli transaction');
+  });
+
+  it('sign', () => {
+    Transaction.createWithCardanoCliBin().sign((builder) => {
+      expect(builder).toBeInstanceOf(SignOptions);
+      //return is not important
+      return builder.withOutFile((b) => b.createForFile(''));
+    });
+  });
+
+  it('submit', () => {
+    Transaction.createWithCardanoCliBin().submit((builder) => {
+      expect(builder).toBeInstanceOf(SubmitOptions);
+      //return is not important
+      return builder.withTxFile('');
+    });
+  });
+
+  it('build', () => {
+    Transaction.createWithCardanoCliBin().build((builder) => {
+      expect(builder).toBeInstanceOf(BuildOptions);
+      //return is not important
+      return builder.withTxIn((b) => b.withTxIn(''));
+    });
   });
 });

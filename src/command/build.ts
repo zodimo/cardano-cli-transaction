9c4fec7117d7671dc4311472775cd3bf0d5d0b59
Jaco Labuschagne <zodimo@gmail.com>
2023-01-15 09:06:07 +0200

    tx-in-parameter and tests

--- src/command/build.ts
+++ src/command/build.ts
@@ -1,4 +1,5 @@
 import {
+  BooleanCommandParameter,
   Builder,
   Command,
   CommandOptions,
@@ -38,9 +39,208 @@ export class BuildOutput extends StringCommandParameter {
   }
 }
 
-export class TxInParameter extends CompositeCommandParameter {
+export class TxInParameterBuilder {
   withTxIn(value: string): TxInParameter {
-    this.withParameter(new StringCommandParameter('tx-in', value));
+    return TxInParameter.fromTxIn(value);
+  }
+}
+
+export class SpendingReferenceTxInDatumBuilder {
+  cborFile(value: string): SpendingReferenceTxInDatum {
+    return SpendingReferenceTxInDatum.cborFile(value);
+  }
+  jsonFile(value: string): SpendingReferenceTxInDatum {
+    return SpendingReferenceTxInDatum.jsonFile(value);
+  }
+  jsonValue(value: string): SpendingReferenceTxInDatum {
+    return SpendingReferenceTxInDatum.jsonValue(value);
+  }
+  inlineDatumIsPresent(): SpendingReferenceTxInDatum {
+    return SpendingReferenceTxInDatum.inlineDatumIsPresent();
+  }
+}
+
+export class SpendingReferenceTxInDatum extends CompositeCommandParameter {
+  static cborFile(value: string): SpendingReferenceTxInDatum {
+    const param = 'spending-reference-tx-in-datum-cbor-file';
+    return SpendingReferenceTxInDatum.from(StringCommandParameter.from(param, value));
+  }
+  static jsonFile(value: string): SpendingReferenceTxInDatum {
+    const param = 'spending-reference-tx-in-datum-file';
+    return SpendingReferenceTxInDatum.from(StringCommandParameter.from(param, value));
+  }
+  static jsonValue(value: string): SpendingReferenceTxInDatum {
+    const param = 'spending-reference-tx-in-datum-value';
+    return SpendingReferenceTxInDatum.from(StringCommandParameter.from(param, value, true));
+  }
+  static inlineDatumIsPresent(): SpendingReferenceTxInDatum {
+    const param = 'spending-reference-tx-in-inline-datum-present';
+    return SpendingReferenceTxInDatum.from(BooleanCommandParameter.from(param));
+  }
+}
+
+export class SpendingReferenceTxInRedeemerBuilder {
+  // this is a factory instance not a builder
+  cborFile(value: string): SpendingReferenceTxInRedeemer {
+    return SpendingReferenceTxInRedeemer.cborFile(value);
+  }
+  jsonFile(value: string): SpendingReferenceTxInRedeemer {
+    return SpendingReferenceTxInRedeemer.jsonFile(value);
+  }
+  jsonValue(value: string): SpendingReferenceTxInRedeemer {
+    return SpendingReferenceTxInRedeemer.jsonValue(value);
+  }
+}
+
+export class SpendingReferenceTxInRedeemer extends CompositeCommandParameter {
+  // this is a factory class not a builder
+  static cborFile(value: string): SpendingReferenceTxInRedeemer {
+    const param = 'spending-reference-tx-in-redeemer-cbor-file';
+    return SpendingReferenceTxInRedeemer.from(StringCommandParameter.from(param, value));
+  }
+  static jsonFile(value: string): SpendingReferenceTxInRedeemer {
+    const param = 'spending-reference-tx-in-redeemer-file';
+    return SpendingReferenceTxInRedeemer.from(StringCommandParameter.from(param, value));
+  }
+  static jsonValue(value: string): SpendingReferenceTxInRedeemer {
+    const param = 'spending-reference-tx-in-redeemer-value';
+    return SpendingReferenceTxInRedeemer.from(StringCommandParameter.from(param, value));
+  }
+}
+export class TxInScriptDatumBuilder {
+  cborFile(value: string): TxInScriptDatum {
+    return TxInScriptDatum.cborFile(value);
+  }
+  jsonFile(value: string): TxInScriptDatum {
+    return TxInScriptDatum.jsonFile(value);
+  }
+  jsonValue(value: string): TxInScriptDatum {
+    return TxInScriptDatum.jsonValue(value);
+  }
+  inlineDatumIsPresent(): TxInScriptDatum {
+    return TxInScriptDatum.inlineDatumIsPresent();
+  }
+}
+
+export class TxInScriptDatum extends CompositeCommandParameter {
+  static cborFile(value: string): TxInScriptDatum {
+    const param = 'tx-in-datum-cbor-file';
+    return TxInScriptDatum.from(StringCommandParameter.from(param, value));
+  }
+  static jsonFile(value: string): TxInScriptDatum {
+    const param = 'tx-in-datum-file';
+    return TxInScriptDatum.from(StringCommandParameter.from(param, value));
+  }
+  static jsonValue(value: string): TxInScriptDatum {
+    const param = 'tx-in-datum-value';
+    return TxInScriptDatum.from(StringCommandParameter.from(param, value, true));
+  }
+  static inlineDatumIsPresent(): TxInScriptDatum {
+    const param = 'tx-in-inline-datum-present';
+    return TxInScriptDatum.from(BooleanCommandParameter.from(param));
+  }
+}
+
+export class TxInScriptRedeemerBuilder {
+  cborFile(value: string): TxInScriptRedeemer {
+    return TxInScriptRedeemer.cborFile(value);
+  }
+  jsonFile(value: string): TxInScriptRedeemer {
+    return TxInScriptRedeemer.jsonFile(value);
+  }
+  jsonValue(value: string): TxInScriptRedeemer {
+    return TxInScriptRedeemer.jsonValue(value);
+  }
+}
+
+export class TxInScriptRedeemer extends CompositeCommandParameter {
+  static cborFile(value: string): TxInScriptRedeemer {
+    const param = 'tx-in-redeemer-cbor-file';
+    return TxInScriptRedeemer.from(StringCommandParameter.from(param, value));
+  }
+  static jsonFile(value: string): TxInScriptRedeemer {
+    const param = 'tx-in-redeemer-file';
+    return TxInScriptRedeemer.from(StringCommandParameter.from(param, value));
+  }
+  static jsonValue(value: string): TxInScriptRedeemer {
+    const param = 'tx-in-redeemer-value';
+    return TxInScriptRedeemer.from(StringCommandParameter.from(param, value, true));
+  }
+}
+
+export class TxInScript extends CompositeCommandParameter {
+  withScriptFile(value: string): TxInScript {
+    this.withParameter(StringCommandParameter.from('tx-in-script-file', value));
+    return this;
+  }
+  withDatum(builder: Builder<TxInScriptDatumBuilder, TxInScriptDatum>): TxInScript {
+    this.withParameter(builder(new TxInScriptDatumBuilder()));
+    return this;
+  }
+  withRedeemer(builder: Builder<TxInScriptRedeemerBuilder, TxInScriptRedeemer>): TxInScript {
+    this.withParameter(builder(new TxInScriptRedeemerBuilder()));
+    return this;
+  }
+}
+
+export class TxInAlternativeBuilder {
+  txInSpending(builder: Builder<TxInSpending, TxInSpending>): TxInAlternative {
+    return TxInAlternative.txInSpending(builder);
+  }
+
+  simpleScriptTxInReference(value: string): TxInAlternative {
+    return TxInAlternative.simpleScriptTxInReference(value);
+  }
+  txInScript(builder: Builder<TxInScript, TxInScript>): TxInAlternative {
+    return TxInAlternative.txInScript(builder);
+  }
+}
+
+export class TxInSpending extends CompositeCommandParameter {
+  withspendingTxInReference(value: string): TxInSpending {
+    this.withParameter(StringCommandParameter.from('spending-tx-in-reference', value));
+    return this;
+  }
+  withSpendingPlutusScriptV2(): TxInSpending {
+    this.withParameter(BooleanCommandParameter.from('spending-plutus-script-v2'));
+    return this;
+  }
+  withSpendingReferenceTxInDatum(
+    builder: Builder<SpendingReferenceTxInDatumBuilder, SpendingReferenceTxInDatum>,
+  ): TxInSpending {
+    this.withParameter(builder(new SpendingReferenceTxInDatumBuilder()));
+    return this;
+  }
+
+  withSpendingReferenceTxInRedeemer(
+    builder: Builder<SpendingReferenceTxInRedeemerBuilder, SpendingReferenceTxInRedeemer>,
+  ): TxInSpending {
+    this.withParameter(builder(new SpendingReferenceTxInRedeemerBuilder()));
+    return this;
+  }
+}
+
+export class TxInAlternative extends CompositeCommandParameter {
+  static txInSpending(builder: Builder<TxInSpending, TxInSpending>): TxInAlternative {
+    return TxInAlternative.from(builder(new TxInSpending()));
+  }
+
+  static simpleScriptTxInReference(value: string): TxInAlternative {
+    return TxInAlternative.from(StringCommandParameter.from('simple-script-tx-in-reference', value));
+  }
+
+  static txInScript(builder: Builder<TxInScript, TxInScript>): TxInAlternative {
+    return TxInAlternative.from(builder(new TxInScript()));
+  }
+}
+
+export class TxInParameter extends CompositeCommandParameter {
+  static fromTxIn(value: string): TxInParameter {
+    return new TxInParameter(new StringCommandParameter('tx-in', value));
+  }
+
+  withAlternative(builder: Builder<TxInAlternativeBuilder, TxInAlternative>): TxInParameter {
+    this.withParameter(builder(new TxInAlternativeBuilder()));
     return this;
   }
 }
@@ -91,9 +291,9 @@ export class BuildOptions implements CommandOptions {
     return this;
   }
 
-  withTxIn(builder: Builder<TxInParameter, TxInParameter>): BuildOptions {
+  withTxIn(builder: Builder<TxInParameterBuilder, TxInParameter>): BuildOptions {
     assert(typeof builder == 'function');
-    this.txIns.push(builder(new TxInParameter()));
+    this.txIns.push(builder(new TxInParameterBuilder()));
     return this;
   }
   withChangeAddress(value: string): BuildOptions {

80708c84c76dffe5efacd2543a83f2ffd21915b0
Jaco Labuschagne <zodimo@gmail.com>
2023-01-15 10:09:10 +0200

    build-output tests

---
+++ test/command/build/buildParameters/build-output.spec.ts
@@ -0,0 +1,10 @@
+import { BuildOutput } from '../../../../src/command/buildParameters/build-output';
+describe('transaction build outputs', () => {
+  //(--out-file FILE | --calculate-plutus-script-cost FILE)
+  it('out-file', () => {
+    expect(BuildOutput.outFile('my-file').toString()).toBe(`--out-file my-file`);
+  });
+  it('calculate-plutus-script-cost', () => {
+    expect(BuildOutput.calculatePlutusScriptCost('my-file').toString()).toBe(`--calculate-plutus-script-cost my-file`);
+  });
+});

5b3cb7aac76ae212f28bb7949f49eaf6ff5a5c40
Jaco Labuschagne <zodimo@gmail.com>
2023-01-16 10:11:22 +0200

    pass options instead of builder

--- test/command/policy-id.spec.ts
+++ test/command/policy-id.spec.ts
@@ -1,13 +1,10 @@
+import { PolicyIdOptions } from '../../src/command/policy-id';
 import { Transaction } from '../../src/transaction';
 
 describe('policy-id', () => {
   // Usage: cardano-cli transaction policyid --script-file FILE
   it('command name', () => {
-    expect(
-      Transaction.createWithCardanoCliBin()
-        .policyId((b) => b)
-        .getCommandName(),
-    ).toBe('policyid');
+    expect(Transaction.createWithCardanoCliBin().policyId(new PolicyIdOptions()).getCommandName()).toBe('policyid');
   });
 
   it('script-file', () => {

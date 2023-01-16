8455cb45bf86e61c2f3218443580ec9f39f070eb
Jaco Labuschagne <zodimo@gmail.com>
2023-01-16 15:13:10 +0200

    exports

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export * from './command/policy-id';
 export * from './command/sign';
 export * from './command/submit';
 export * from './command/calculate-min-fee';
+export * from './command/tx-id';
